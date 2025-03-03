6cb45b9a73e5c7e68fa8d9ce3e95c83f3d6d282b
rohansinha01 <rohansinha01@gmail.com>
2025-03-03 15:58:07 -0500

    int practice with floats

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -4,4 +4,10 @@ import "fmt"
 func main(){
 	var intNum int16 = 32767
 	fmt.Println(intNum)
+
+	var floatNum float32 = 12345678.9
+	fmt.Println(floatNum)
+
+	var floatNum2 float64 = 12345678.9
+	fmt.Println(floatNum2)
 }
