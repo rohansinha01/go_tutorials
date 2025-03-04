b721980c59468b83c286d86160be07f54260b329
rohansinha01 <rohansinha01@gmail.com>
2025-03-04 13:39:24 -0500

    intNum practice

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -15,4 +15,9 @@ func main(){
 	var intNum32 int32 = 2
 	var result float32 = floatNum32 + float32(intNum32)
 	fmt.Println(result)
+
+	var intNum1 int = 3
+	var intNum2 int = 2
+	fmt.Println(intNum1/intNum2)
+	fmt.Println(intNum1%intNum2)
 }
